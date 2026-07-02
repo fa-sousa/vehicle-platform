f7f82396002f5273a21a5947118118b9fd13727a
fa-sousa <fsousa292@gmail.com>
2026-07-02 17:47:56 -0300

    feat: initial spring boot setup

---
+++ src/test/kotlin/com/fasousa/vehicleplatform/VehiclePlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fasousa.vehicleplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VehiclePlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
