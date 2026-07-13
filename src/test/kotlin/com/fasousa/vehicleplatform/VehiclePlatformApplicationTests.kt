a3ec414a1da39a0ffd388a5f648f51d713f7a4ad
fa-sousa <fsousa292@gmail.com>
2026-07-12 23:58:09 -0300

    Refactor configuration files and adjust project

--- src/test/kotlin/com/fasousa/vehicleplatform/VehiclePlatformApplicationTests.kt
+++ src/test/kotlin/com/fasousa/vehicleplatform/VehiclePlatformApplicationTests.kt
@@ -1,16 +1,24 @@
 package com.fasousa.vehicleplatform
 
-import org.junit.jupiter.api.Test
+import com.fasousa.vehicleplatform.infrastructure.persistence.repository.PaymentEventMongoRepository
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import kotlin.test.Test
 
 @SpringBootTest(
 	properties = [
-		"spring.data.mongodb.uri=mongodb://localhost:27017/vehicle_platform_test",
+		"server.port=0",
 		"vehicle-sale-service.base-url=http://localhost:8080"
 	]
 )
+@ActiveProfiles("test")
 class VehiclePlatformApplicationTests {
 
+	@MockitoBean
+	lateinit var paymentEventMongoRepository: PaymentEventMongoRepository
+
 	@Test
-	fun contextLoads() {}
+	fun contextLoads() {
+	}
 }
